5268fc18fae7e0faa1e848460633523f02a2f8fd
bruno1902 <brunodileo@live.com>
2018-12-26 23:46:43 +0000

    Exercicios OO

---
+++ Orientação a objetos/Exercicio -02/Exercicio -02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio__02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionarios Funcionario1 = new Funcionarios();
+            Funcionarios Funcionario2 = new Funcionarios();
+
+            double salariomedio;
+
+            Console.WriteLine("Digite o nome do fguncionario1 ");
+            Funcionario1.nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario: ");
+            Funcionario1.salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o nome do funcionario2 ");
+            Funcionario2.nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario: ");
+            Funcionario2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salariomedio = (Funcionario1.salario + Funcionario2.salario) / 2;
+
+            Console.WriteLine("Salario medio = " + salariomedio.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
